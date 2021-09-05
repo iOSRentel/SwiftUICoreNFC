583d6cfa26664a713fd20cf24fe5cf699fd76750
iOSRentel <88737803+iOSRentel@users.noreply.github.com>
2021-09-05 12:49:31 +0300

    Initial Commit

---
+++ SwiftUICoreNFC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICoreNFC
+//
+//  Created by Boris Zinovyev on 05.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICoreNFC/SwiftUICoreNFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoreNFCApp.swift
+//  SwiftUICoreNFC
+//
+//  Created by Boris Zinovyev on 05.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoreNFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
